0e54e05bb8ddbb5ba260778b0ecc309fdcf83ea2
CliffLinus Thomi . <thomilinus5@gmail.com>
2025-06-24 15:32:33 +0300

    Update seed.py

--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,9 @@
-# server/seed.py
-from server.app import create_app, db
-from server.models import Guest, Episode, Appearance, User
-from datetime import date
+from models import db
+from server.models.guest import Guest
+from server.models.episode import Episode
+from server.models.appearance import Appearance
+from flask import Flask
+from server.app import create_app
 
 app = create_app()
 
@@ -9,26 +11,17 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
-    # Create Users
-    user = User(username="admin")
-    user.set_password("password")
-    db.session.add(user)
+    guest1 = Guest(name="Keanu Reeves", occupation="Actor")
+    guest2 = Guest(name="Taylor Swift", occupation="Singer")
+    
+    ep1 = Episode(date="2023-06-01", number=101)
+    ep2 = Episode(date="2023-06-02", number=102)
 
-    # Create Guests
-    guest1 = Guest(name="Tom Hanks", occupation="Actor")
-    guest2 = Guest(name="Adele", occupation="Singer")
-    db.session.add_all([guest1, guest2])
-
-    # Create Episodes
-    episode1 = Episode(date=date(2024, 6, 1), number=1)
-    episode2 = Episode(date=date(2024, 6, 2), number=2)
-    db.session.add_all([episode1, episode2])
-
-    # Create Appearances
-    appearance1 = Appearance(rating=5, guest=guest1, episode=episode1)
-    appearance2 = Appearance(rating=4, guest=guest2, episode=episode1)
-    appearance3 = Appearance(rating=3, guest=guest1, episode=episode2)
-    db.session.add_all([appearance1, appearance2, appearance3])
-
+    db.session.add_all([guest1, guest2, ep1, ep2])
     db.session.commit()
-    print("Database seeded successfully.")
+
+    app1 = Appearance(rating=5, guest_id=guest1.id, episode_id=ep1.id)
+    app2 = Appearance(rating=4, guest_id=guest2.id, episode_id=ep2.id)
+
+    db.session.add_all([app1, app2])
+    db.session.commit()
